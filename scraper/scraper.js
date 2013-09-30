8d7966afcf10d31b2c5bd068d886c323849188c7
Eduardo Ramírez <ejramire@uc.cl>
2013-09-30 14:29:10 -0300

    To Do

---
+++ scraper/scraper.js
@@ -0,0 +1,5 @@
+// instaciar scraper (requier node crawler etc etc)
+//manejar threads
+//definir metodos para leer html de carpeta (lo haria en otro archivo, getFiles.js o algo)
+//sacar info de threads(tambien otro archivo, process.js)
+//guardar en bdd(store.js)
