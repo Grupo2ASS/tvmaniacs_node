fb6885a41aeaf79cc11949348f1351c84897fdc1
Eduardo Ramírez <ejramire@uc.cl>
2013-10-05 15:02:18 -0300

    #1128812

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,6 +1,3 @@
-
-
-
 //Podriamos pasar parametros por linea de comandos, la carpeta dnd estan los html, el .js dnd esta el acceso a la bdd
 // (por los passwords y eso) y la cantidad de tiempo de cada cuanto rato revisa si hay
 //nuevos archivos.
@@ -15,7 +12,7 @@ var imdbActorProcesser = require('./imdb_actor');
 var html_actor = fs.readFileSync('./html_test_files/imdb/actors/imdb_actor.html')
 var actor = imdbActorProcesser.getInfo(html_actor);
 console.log("Actor Info:")
-console.log(actor); 
+console.log(actor);
 
 //obtener links de actor imdb. Check!
 var links_actor = imdbActorProcesser.getLinks(html_actor);
@@ -28,9 +25,10 @@ var imdbSeriesProcesser = require('./imdb_series');
 var html_series = fs.readFileSync('./html_test_files/imdb/series/imdb_series.html')
 var series = imdbSeriesProcesser.getInfo(html_series);
 console.log("Series Info:")
-console.log(series); 
+console.log(series);
 
 
-var links_series = imdbActorProcesser.getLinks(html_series);
+
+var links_series = imdbSeriesProcesser.getLinks(html_series);
 console.log("Series Links:")
 console.log(links_series);
